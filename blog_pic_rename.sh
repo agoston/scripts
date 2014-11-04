9ed77df45a56eb3fca07131c6a73231d216168f4
Agoston Horvath <ahorvath@bol.com>
2014-11-05 00:19:15 +0100

    fixed bug of noticing itself as existing other file

--- blog_pic_rename.sh
+++ blog_pic_rename.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 for i in "$@"; do
-	if ! [[ "$i" =~ img_[0-9]*_[0-9]*.*\.jpe?g ]]; then
-		NEWNAME=$(exiftime "$i" | tr ':' ' ' | gawk '{printf("img_%s%s%s_%s%s%s.jpg", $3, $4, $5, $6, $7, $8); exit}')
-		# only rename if exif is right
-		if [[ $NEWNAME =~ img_[0-9]{8}_[0-9]{6}.jpg ]]; then
-			# find unused filename
-			COUNT=2
-			while [[ -e $NEWNAME ]]; do
-				NEWNAME=${NEWNAME:0:19}_${COUNT}.jpg
-				COUNT=$((COUNT+1))
-			done
-			mv -n -v "$i" $NEWNAME 1>&2
-		fi
+	NEWNAME=$(exiftime "$i" | tr ':' ' ' | gawk '{printf("img_%s%s%s_%s%s%s.jpg", $3, $4, $5, $6, $7, $8); exit}')
+	# only rename if exif is right
+	if [[ $NEWNAME =~ img_[0-9]{8}_[0-9]{6}.jpg ]]; then
+		# self?
+		if [[ "$NEWNAME" == "$i" ]]; then continue; fi
+
+		# find unused filename
+		COUNT=2
+		while [[ -e $NEWNAME ]]; do
+			NEWNAME=${NEWNAME:0:19}_${COUNT}.jpg
+			COUNT=$((COUNT+1))
+		done
+		mv -n -v "$i" $NEWNAME 1>&2
 	fi
 done
