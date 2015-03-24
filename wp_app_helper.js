93bd6d4fe6daa46b951587b9a7e75d1c20c2eb4c
Agoston Horvath <ahorvath@bol.com>
2015-03-24 23:59:09 +0100

    moved client config to local config

--- wp_app_helper.js
+++ wp_app_helper.js
@@ -6,12 +6,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var xmlrpc = require('xmlrpc');
 var config = require('./wp_config.js');
 
-var client = xmlrpc.createSecureClient({
-    host: 'seagoat.xs4all.nl',
-    port: 443,
-    path: '/wordpress/xmlrpc.php',
-    basic_auth: config.http_auth
-});
+var client = xmlrpc.createSecureClient(config.client);
 
 client.methodCall('wp.getPostTypes', [0, config.wordpress.username, config.wordpress.password], function (error, value) {
     console.log(value);
