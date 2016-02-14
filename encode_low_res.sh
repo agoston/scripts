67f212042bff36fa8f3bcc7720062c68393edaae
Agoston Horvath <horvath.agoston@gmail.com>
2016-02-14 23:49:27 +0100

    keep aspect ratio for encode_lowres

--- encode_low_res.sh
+++ encode_low_res.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -x
-exec ffmpeg -i $1 -vf scale="-1:'if(gt(ih,600),600,ih)'" -c:v libvpx-vp9 -b:v 1280K -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm -threads 4 ${1%.*}.webm
+
+# kindle fire 5th gen
+XRES=1024
+YRES=600
+
+
+exec ffmpeg -i $1 -vf scale="'if(gt(a,${XRES}/${YRES}),${XRES},-1)':'if(gt(a,${XRES}/${YRES}),-1,${YRES})'" -c:v libvpx-vp9 -b:v 1280K -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm -threads 4 ${1%.*}.webm
