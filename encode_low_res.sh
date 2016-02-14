601c7ee2019b69d2dd2d309bc5abce8e1e13b837
Agoston Horvath <horvath.agoston@gmail.com>
2016-02-15 00:18:24 +0100

    added no upscaling

--- encode_low_res.sh
+++ encode_low_res.sh
@@ -5,4 +5,4 @@ XRES=1024
 YRES=600
 
 
-exec ffmpeg -i $1 -vf scale="'if(gt(a,${XRES}/${YRES}),${XRES},-1)':'if(gt(a,${XRES}/${YRES}),-1,${YRES})'" -c:v libvpx-vp9 -b:v 1280K -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm -threads 4 ${1%.*}.webm
+exec ffmpeg -i $1 -vf scale="'if(gt(a,${XRES}/${YRES}),if(gt(ih,${XRES}),${XRES},-1),-1)':'if(gt(a,${XRES}/${YRES}),-1,if(gt(iw,${YRES}),${YRES},-1))'" -c:v libvpx-vp9 -b:v 1280K -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm -threads 4 ${1%.*}.webm
