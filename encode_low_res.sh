b10c259951d9cfa25d39fbe5ab5b05287ae7bb4d
Agoston Horvath <horvath.agoston@gmail.com>
2016-03-10 19:56:25 +0100

    added further notes

--- encode_low_res.sh
+++ encode_low_res.sh
@@ -9,7 +9,5 @@ exec ffmpeg -i "$1" -vf scale="'if(gt(a,${XRES}/${YRES}),if(gt(ih,${XRES}),${XRE
 exit 0
 
 # unpacked DVD (check out '-vf yadif' !!!) (same also works for .iso image, without '-i concat:' then of course)
-for i in Buurman.en.Buurman.DVD2.iso Buurman.en.Buurman.DVD5.iso Buurman.en.Buurman.DVD6.iso; do ffmpeg -i concat:$(ls -1 ${i}/VIDEO_TS/VTS_*_1.VOB | perl -ape 's/\n/|/g' | sed 's/|$//') -vf yadif -c:v libvpx-vp9 -b:v 2560K -g 80 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 64k -f webm ${i%.*}.webm; done
-
-# same with crf=23 (for semi-constant quality; not really usable, bitrate floats around 3500K regardless of crf)
-ffmpeg -i XXX -vf yadif -c:v libvpx-vp9 -b:v 2560K -g 80 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 96k -f webm XXX.webm2
+# (-crf does not seem to work -- like it was ignored or something; use -b:v with libvpx)
+for i in Buurman.en.Buurman.DVD2.iso Buurman.en.Buurman.DVD5.iso Buurman.en.Buurman.DVD6.iso; do ffmpeg -i concat:$(ls -1 ${i}/VIDEO_TS/VTS_*_1.VOB | perl -ape 's/\n/|/g' | sed 's/|$//') -vf yadif -c:v libvpx-vp9 -b:v 2560K -g 80 -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a 96k -f webm ${i%.*}.webm; done
