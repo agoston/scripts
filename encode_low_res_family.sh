dd2cbe5708378e47ddc44af07a4980c87b997b7e
Agoston Horvath <horvath.agoston@gmail.com>
2016-07-31 23:45:11 +0200

    added hard link support to encode_family

--- encode_low_res_family.sh
+++ encode_low_res_family.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/bin/bash -x
 # thread-safe!
+# supports hard links!
 
 FAMILY_DIR=/net/seagoat/home/ftp/video/Family
 
 IFS=$'\n'
 for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' </net/seagoat/home/ftp/www/webplay/index.html); do
-	OLD="$FAMILY_DIR/$i"
-	NEW="${OLD%.*}.webm"
-	if ! [[ -e "$NEW" ]]; then
-		encode_low_res.sh "$OLD"
+	NEW="${i%.*}.webm"
+	if ! [[ -e "$FAMILY_DIR/$NEW" ]]; then
+		# check for hard link & continue outer cycle if found
+		while read SAMEFILE; do
+			SAMENEW="${SAMEFILE%.*}.webm";
+			if [[ -e "$SAMENEW" ]]; then
+				ln "$SAMENEW" "$NEW"
+				continue 2
+			fi
+		done < <(find $FAMILY_DIR -samefile "$i" -a '!' -name "$i")
+		
+		# no hard link, go encode
+		encode_low_res.sh "$FAMILY_DIR/$i"
 	fi
 done
