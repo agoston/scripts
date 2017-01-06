efb0d61babce0a136cd1256999b3280d7fdc7356
Agoston Horvath <horvath.agoston@gmail.com>
2017-01-06 21:12:18 +0100

    added sudo to failed poweroff script (meh :)

--- encode_low_res_family.sh
+++ encode_low_res_family.sh
@@ -3,9 +3,10 @@
 # supports hard links!
 
 FAMILY_DIR=/net/seagoat/home/ftp/video/Family
+WEBPLAY=/net/seagoat/home/ftp/www/webplay/index.html
 
 IFS=$'\n'
-for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' </net/seagoat/home/ftp/www/webplay/index.html); do
+for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' <$WEBPLAY); do
 	NEW="${i%.*}.webm"
 	if ! [[ -e "$FAMILY_DIR/$NEW" ]]; then
 		# check for hard link & continue outer cycle if found
