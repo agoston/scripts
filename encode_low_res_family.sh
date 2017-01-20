18afae888377703ff8b111104af525017ae5b3fb
Agoston Horvath <horvath.agoston@gmail.com>
2017-01-20 10:59:37 +0100

    fixed hard link detection & handling

--- encode_low_res_family.sh
+++ encode_low_res_family.sh
@@ -5,10 +5,12 @@
 FAMILY_DIR=/net/seagoat/home/ftp/video/Family
 WEBPLAY=/net/seagoat/home/ftp/www/webplay/index.html
 
+cd $FAMILY_DIR
+
 IFS=$'\n'
 for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' <$WEBPLAY); do
 	NEW="${i%.*}.webm"
-	if ! [[ -e "$FAMILY_DIR/$NEW" ]]; then
+	if ! [[ -e "$NEW" ]]; then
 		# check for hard link & continue outer cycle if found
 		while read SAMEFILE; do
 			SAMENEW="${SAMEFILE%.*}.webm";
@@ -16,9 +18,9 @@ for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' <$WEBPLAY);
 				ln "$SAMENEW" "$NEW"
 				continue 2
 			fi
-		done < <(find $FAMILY_DIR -samefile "$i" -a '!' -name "$i")
+		done < <(find -samefile "$i" -a '!' -name "$i")
 		
 		# no hard link, go encode
-		encode_low_res.sh "$FAMILY_DIR/$i"
+		encode_low_res.sh "$i"
 	fi
 done
