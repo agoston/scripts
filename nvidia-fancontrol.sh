74da4775b62c57f474fc619953999449d990c8b4
Agoston Horvath <horvath.agoston@gmail.com>
2017-11-29 11:37:01 +0100

    merge home

--- nvidia-fancontrol.sh
+++ nvidia-fancontrol.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=82' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=500' -a '[gpu:0]/GPUGraphicsClockOffset[3]=120'
+#nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=82' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=500' -a '[gpu:0]/GPUGraphicsClockOffset[3]=120'
+nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=85' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1000' -a '[gpu:0]/GPUGraphicsClockOffset[3]=-200'
