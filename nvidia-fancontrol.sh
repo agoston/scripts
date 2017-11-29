d616bcf7b33ff91a51d2abafe0ee823476447f0f
Agoston Horvath <horvath.agoston@gmail.com>
2017-11-30 00:24:00 +0100

    fine-tune overclock for ethminer

--- nvidia-fancontrol.sh
+++ nvidia-fancontrol.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=82' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=500' -a '[gpu:0]/GPUGraphicsClockOffset[3]=120'
-nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=85' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=1000' -a '[gpu:0]/GPUGraphicsClockOffset[3]=-200'
+nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=87' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=750' -a '[gpu:0]/GPUGraphicsClockOffset[3]=-100'
