d1b9d233638d32f46a48cad3967d2853a57a6db3
Agoston Horvath <horvath.agoston@gmail.com>
2018-01-03 21:30:39 +0100

    further overclock

--- nvidia-fancontrol.sh
+++ nvidia-fancontrol.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Enable persistence mode
+sudo nvidia-smi -i 0 -pm 1
+
+# Increase our Power Limit
+sudo nvidia-smi -i 0 -pl 170
+
 #nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=82' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=500' -a '[gpu:0]/GPUGraphicsClockOffset[3]=120'
-nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=87' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=750' -a '[gpu:0]/GPUGraphicsClockOffset[3]=-100'
+nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=90' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=900' -a '[gpu:0]/GPUGraphicsClockOffset[3]=0'
