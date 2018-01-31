41d90e44f39b46cedd38abf3065ec8bad33af373
Agoston Horvath <horvath.agoston@gmail.com>
2018-01-31 23:41:14 +0100

    moved nvidia-smi to root's crontab

--- nvidia-fancontrol.sh
+++ nvidia-fancontrol.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Enable persistence mode
-sudo nvidia-smi -i 0 -pm 1
-
-# Increase our Power Limit
-sudo nvidia-smi -i 0 -pl 170
-
 #nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=82' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=500' -a '[gpu:0]/GPUGraphicsClockOffset[3]=120'
-nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=88' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=900' -a '[gpu:0]/GPUGraphicsClockOffset[3]=0'
+nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=89' -a '[gpu:0]/GPUMemoryTransferRateOffset[3]=900' -a '[gpu:0]/GPUGraphicsClockOffset[3]=0'
