ac8d04b182fafd464e6999abbd36250f9d8052aa
Agoston Horvath <horvath.agoston@gmail.com>
2018-02-01 10:24:53 +0100

    fix .. hack

--- encode_low_res_family.sh
+++ encode_low_res_family.sh
@@ -12,6 +12,7 @@ for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' <$WEBPLAY);
 	NEW="${i%.*}.webm"
 	
 	# check if mtime between 1-14 days and no duration
+	# FIXME: latest ffmpeg fills duration, compare if much shorter too!
 	if [ $(find . -maxdepth 1 -type f -name "$NEW" -mmin +120 -mtime -14) ]; then
 		if ffprobe -show_format "$NEW" 2>/dev/null | grep -q 'duration=N/A'; then
 			rm "$NEW"
