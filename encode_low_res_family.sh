e8907fcc1f73d531957cf8253b2530e0878b43b3
Agoston Horvath <horvath.agoston@gmail.com>
2018-02-01 10:59:49 +0100

    adjusted encode for ..

--- encode_low_res_family.sh
+++ encode_low_res_family.sh
@@ -2,7 +2,7 @@
 # thread-safe!
 # supports hard links!
 
-FAMILY_DIR=/net/seagoat/home/ftp/video/Family
+FAMILY_DIR=/net/seagoat/home/ftp/video
 WEBPLAY=/net/seagoat/home/ftp/www/webplay/index.html
 
 cd $FAMILY_DIR
@@ -13,7 +13,7 @@ for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' <$WEBPLAY);
 	
 	# check if mtime between 1-14 days and no duration
 	# FIXME: latest ffmpeg fills duration, compare if much shorter too!
-	if [ $(find . -maxdepth 1 -type f -name "$NEW" -mmin +120 -mtime -14) ]; then
+	if [ $(find . -maxdepth 1 -type f -wholename "$NEW" -mmin +120 -mtime -14) ]; then
 		if ffprobe -show_format "$NEW" 2>/dev/null | grep -q 'duration=N/A'; then
 			rm "$NEW"
 		fi
@@ -27,9 +27,9 @@ for i in $(perl -ale 'if (/.* movie-file="([^.].*?)".*/) {print $1}' <$WEBPLAY);
 				ln "$SAMENEW" "$NEW"
 				continue 2
 			fi
-		done < <(find -samefile "$i" -a '!' -name "$i")
+		done < <(find -samefile "$i" -a '!' -wholename "$i")
 		
 		# no hard link, go encode
-		encode_low_res.sh "$i"
+		echo encode_low_res.sh "$i"
 	fi
 done
