3ad89890ea1586041b75964a53cd240b1f0e8e5a
Agoston Horvath <horvath.agoston@gmail.com>
2018-12-12 23:51:09 +0100

    updated baudrate to a more normal 9600 to avoid garbage. still don't know why this won't work with higher rates - in theory not even 900K should be a problem over usb :(

--- rfm_send.sh
+++ rfm_send.sh
@@ -3,5 +3,11 @@
 DEVICE=/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0
 echo "$(date) $*" &>>$0.log
 
-# no clue why the 2 sec delay is necessary here
-(sleep 2; echo ';'"$*"';') | busybox microcom -d 10 -t 5000 -X -s 57600 $DEVICE &>>$0.log
+(
+echo -n ';' # flush any buffer from read buffer
+echo -n "$*"
+echo -n ';' # send command
+) | busybox microcom -t 5000 -X -s 9600 $DEVICE &>>$0.log
+
+## no clue why the 2 sec delay is necessary here
+#(sleep 2; echo ';'"$*"';') | busybox microcom -d 10 -t 5000 -X -s 57600 $DEVICE &>>$0.log
