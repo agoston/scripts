4da136080499f3452a8dc921c570a184ef65a1b3
Agoston Horvath <horvath.agoston@gmail.com>
2019-02-21 22:25:49 +0100

    implemented rfm mesh protocol in python, works flawlessly, without the continous transmit errors of microcom-based solution

---
+++ rfm.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import serial
+import time
+import sys
+
+DEVICE = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
+ser = serial.Serial(DEVICE, 9600, timeout = 5)
+command = " ".join(sys.argv[1:]) + ";"
+
+if len(sys.argv) <= 1:
+    sys.exit('command not specified')
+
+def response():
+    # first line is atmega echoing back what it received (for spotting transmission errors)
+    echo = ser.readline()
+    if not echo:
+        time.sleep(0.2)
+        return False
+
+    print(">" + echo.decode())
+    
+    # second line is response
+    response = ser.readline()
+    if not response:
+        time.sleep(0.2)
+        return False
+        
+    print(response.decode())
+            
+    if b"." == response:
+        return True
+
+    if b"E T" == response:
+        # transmit error
+        return False
+
+    # other/misc errors (temperature query didn't respond, etc...)
+    return False
+
+# send ; to empty buffer
+ser.write(";".encode("ascii", "ignore"))
+response();
+
+# retry 5 times
+for i in range(5):
+    ser.write(command.encode("ascii", "ignore"))
+    time.sleep(1);
+    if not response():
+        continue
+
+    break        
