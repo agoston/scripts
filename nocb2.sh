aeba7452e2a890fd41fd04cf955f0370e5e8ff6f
Agoston Horvath <ahorvath@bol.com>
2019-05-03 10:05:30 +0200

    added doc

--- nocb2.sh
+++ nocb2.sh
@@ -7,3 +7,9 @@
 
 # keep disabled
 hdiutil attach /opt/nocb.dmg -mountpoint /System/Library/Extensions/CbDefenseSensor.kext
+
+
+# to unload + disable a plist in /Library/Launch*:
+# launchctl unload -w com.blabla.plist
+# to re-enable:
+# launchctl load -w com.blabla.plist
