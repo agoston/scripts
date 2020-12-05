cb12777a13d14a40ffa411bbe80419d1397f441b
Agoston Horvath <horvath.agoston@gmail.com>
2020-12-05 21:04:16 +0100

    added usage

--- ffmpeg_join.sh
+++ ffmpeg_join.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+if [[ $# -lt 1 ]]; then
+	echo "usage: $0 file.mp4 file.mp4 file.mp4"
+	echo "output is written to 'output.mp4'"
+	exit 1
+fi
+
 ffmpeg -f concat -safe 0 -i <(for i in "$@" ; do echo "file '$PWD/$i'"; done) -c copy ~/output.mp4
