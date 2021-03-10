895d114fe9e90dd1d36171c5aeecff7b26cd1ecd
Agoston Horvath <ahorvath@bol.com>
2021-03-10 13:18:30 +0100

    fixes

--- nocb2.sh
+++ nocb2.sh
@@ -2,13 +2,15 @@
 # disable
 #rm -f /opt/nocb.dmg
 #hdiutil create -megabytes 16 -fs HFS+ /opt/nocb
-#hdiutil attach /opt/nocb.dmg -mountpoint /System/Library/Extensions/CbDefenseSensor.kext
+#hdiutil attach /opt/nocb.dmg -mountpoint /Library/Extensions/CbDefenseSensor.kext
 #kextcache -i /
+# -> reboot
 
-# keep disabled
-hdiutil attach /opt/nocb.dmg -mountpoint /System/Library/Extensions/CbDefenseSensor.kext
+### a kextcache -i miatt nem latja a kernel a modult, es nem is tolti be.
+### ezen a ponton rm -rf /Library/Extensions/CbDefenseSensor.kext, majd touch /Library/Extensions/CbDefenseSensor.kext, majd chflags hogy ugy is maradjon
+### kesz!
 
-
+# csak referencia, nem kell
 # to unload + disable a plist in /Library/Launch*:
 # launchctl unload -w com.blabla.plist
 # to re-enable:
--- random_play.sh
+++ random_play.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -x
 
-PLAYER=mw
-
 LIST=.$(basename $0).list
 
 if ! [[ -s $LIST ]]; then
-	find -type f -iregex '.*\.\(mkv\|mp4\|avi\)' | sort -R >$LIST
+	find . -type f -name '*.mkv' | sort -R >$LIST
 fi
 
 FILE="$(head -1 $LIST)"
 tail -n +2 $LIST >$LIST.tmp
 mv $LIST.tmp $LIST
-$PLAYER "$FILE"
+mpv "$FILE"
