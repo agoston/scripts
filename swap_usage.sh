5518773310c588f1c156f91017e0b7e76555e4fa
Agoston Horvath <horvath.agoston@gmail.com>
2021-06-28 09:13:37 +0200

    updated script

--- swap_usage.sh
+++ swap_usage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r
+for file in /proc/*/status ; do awk '/^VmSwap:|^Name:|^Pid:/ {printf $2 " " $3 "\t"} END {print ""}' $file; done | sort -k 3 -n -r
